1cea840258b25a5fec3a98e30141d6a7d0b8b6c5
Jacob Levin <yacob.levin3@gmail.com>
2020-01-30 14:34:17 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Iterative_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter an interger between 1 and 100");
+
+            try
+            {
+                string input = Console.ReadLine();
+
+                int a = int.Parse(input); //setting up varibles for while loops
+                int b = 1; //throw away variable to create a baseliine 
+                while( b <= a)
+                {
+
+                    Console.WriteLine("You have entered "+ a + "This is the current integer value in the loop: " + b);
+                    b++;
+                }
+            }
+
+            catch
+            {//just in case someone mistypes the input
+                Console.WriteLine("Please input a valid interger");
+            }
+        }
+    }
+}
